e0fcad08b3cc961e40b6fd23a11dcd9336a54bcc
Lee <lee.jensen@agilix.com>
2022-11-10 09:24:17 -0700

    fix(deploy) improved script to remove anything deleted

--- deploy.sh
+++ deploy.sh
@@ -1,7 +1,8 @@
+rm -rf ../webprogramming260.github.io/simon-javascript
 mkdir -p ../webprogramming260.github.io/simon-javascript
 cp *.{html,css,js} ../webprogramming260.github.io/simon-javascript
 cp -r assets ../webprogramming260.github.io/simon-javascript
 cd ../webprogramming260.github.io
 git add .
-git commit -am "updating simon-javascript project"
+git commit -am "deploying simon-javascript project"
 git push
