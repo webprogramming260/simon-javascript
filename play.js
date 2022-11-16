156b43afadc352c2a0a98ac1fc330f1e0b89a0ee
Lee <lee.jensen@agilix.com>
2022-11-16 15:02:29 -0700

    enhance(game) added private methods and fields

--- play.js
+++ play.js
@@ -16,89 +16,94 @@ class Button {
 }
 
 class Game {
-  constructor() {
-    this.buttons = new Map();
-    this.allowPlayer = false;
-    this.sequence = [];
-    this.playerPlaybackPos = 0;
+  #buttons;
+  #allowPlayer;
+  #sequence;
+  #playerPlaybackPos;
+  #mistakeSound;
 
-    this.mistakeSound = loadSound("error.mp3");
+  constructor() {
+    this.#buttons = new Map();
+    this.#allowPlayer = false;
+    this.#sequence = [];
+    this.#playerPlaybackPos = 0;
+    this.#mistakeSound = loadSound("error.mp3");
 
     const sounds = ["sound1.mp3", "sound2.mp3", "sound3.mp3", "sound4.mp3"];
     document.querySelectorAll(".game-button").forEach((el, i) => {
       if (i < sounds.length) {
-        this.buttons.set(el.id, new Button(sounds[i], el));
+        this.#buttons.set(el.id, new Button(sounds[i], el));
         el.style.filter = "brightness(50%)";
       }
     });
   }
 
   async pressButton(button) {
-    if (this.allowPlayer) {
-      this.allowPlayer = false;
-      await this.buttons.get(button.id).press();
+    if (this.#allowPlayer) {
+      this.#allowPlayer = false;
+      await this.#buttons.get(button.id).press();
 
-      if (this.sequence[this.playerPlaybackPos].el.id === button.id) {
-        this.playerPlaybackPos++;
-        if (this.playerPlaybackPos === this.sequence.length) {
-          this.playerPlaybackPos = 0;
-          this.addNote();
-          this.updateScore(this.sequence.length - 1);
-          await this.playSequence(500);
+      if (this.#sequence[this.#playerPlaybackPos].el.id === button.id) {
+        this.#playerPlaybackPos++;
+        if (this.#playerPlaybackPos === this.#sequence.length) {
+          this.#playerPlaybackPos = 0;
+          this.#addNote();
+          this.#updateScore(this.#sequence.length - 1);
+          await this.#playSequence(500);
         }
-        this.allowPlayer = true;
+        this.#allowPlayer = true;
       } else {
-        this.saveScore(this.sequence.length - 1);
-        this.mistakeSound.play();
-        await this.buttonDance();
+        this.#saveScore(this.#sequence.length - 1);
+        this.#mistakeSound.play();
+        await this.#buttonDance();
       }
     }
   }
 
   async reset() {
-    this.allowPlayer = false;
-    this.playerPlaybackPos = 0;
-    this.sequence = [];
-    this.updateScore("--");
-    await this.buttonDance(1);
-    this.addNote();
-    await this.playSequence(1000);
-    this.allowPlayer = true;
+    this.#allowPlayer = false;
+    this.#playerPlaybackPos = 0;
+    this.#sequence = [];
+    this.#updateScore("--");
+    await this.#buttonDance(1);
+    this.#addNote();
+    await this.#playSequence(1000);
+    this.#allowPlayer = true;
   }
 
-  async playSequence(delayms = 0) {
+  async #playSequence(delayms = 0) {
     if (delayms > 0) {
       await delay(delayms);
     }
-    for (const btn of this.sequence) {
+    for (const btn of this.#sequence) {
       await btn.press();
     }
   }
 
-  addNote() {
-    const btn = this.getRandomButton();
-    this.sequence.push(btn);
+  #addNote() {
+    const btn = this.#getRandomButton();
+    this.#sequence.push(btn);
   }
 
-  updateScore(score) {
+  #updateScore(score) {
     const scoreEl = document.querySelector("#score");
     scoreEl.textContent = score;
   }
 
-  async buttonDance(laps = 5) {
+  async #buttonDance(laps = 5) {
     for (let step = 0; step < laps; step++) {
-      for (const btn of this.buttons.values()) {
+      for (const btn of this.#buttons.values()) {
         await btn.press(100, false);
       }
     }
   }
 
-  getRandomButton() {
-    let btns = Array.from(this.buttons.values());
-    return btns[Math.floor(Math.random() * this.buttons.size)];
+  #getRandomButton() {
+    let btns = Array.from(this.#buttons.values());
+    return btns[Math.floor(Math.random() * this.#buttons.size)];
   }
 
-  saveScore(score) {
+  #saveScore(score) {
     let userName = localStorage.getItem("userName");
     if (!userName) {
       userName = "unknown";
@@ -108,12 +113,12 @@ class Game {
     if (scoresText) {
       scores = JSON.parse(scoresText);
     }
-    scores = this.updateScores(userName, score, scores);
+    scores = this.#updateScores(userName, score, scores);
 
     localStorage.setItem("scores", JSON.stringify(scores));
   }
 
-  updateScores(userName, score, scores) {
+  #updateScores(userName, score, scores) {
     const date = new Date().toLocaleDateString();
     const newScore = { name: userName, score: score, date: date };
 
@@ -136,16 +141,16 @@ class Game {
 
     return scores;
   }
-
-  delay(milliseconds) {
-    return new Promise((resolve) => {
-      setTimeout(resolve, milliseconds);
-    });
-  }
-
-  loadSound(filename) {
-    return new Audio("assets/" + filename);
-  }
 }
 
 const game = new Game();
+
+function delay(milliseconds) {
+  return new Promise((resolve) => {
+    setTimeout(resolve, milliseconds);
+  });
+}
+
+function loadSound(filename) {
+  return new Audio("assets/" + filename);
+}
