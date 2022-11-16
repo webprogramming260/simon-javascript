396eb6a1d2d0c38e2d74375da33beec7ad64b2eb
Lee <lee.jensen@agilix.com>
2022-11-16 15:55:02 -0700

    add(favicon)

--- deploy.sh
+++ deploy.sh
@@ -1,6 +1,6 @@
 rm -rf ../webprogramming260.github.io/simon-javascript
 mkdir -p ../webprogramming260.github.io/simon-javascript
-cp *.{html,css,js} ../webprogramming260.github.io/simon-javascript
+cp *.{html,css,js,ico} ../webprogramming260.github.io/simon-javascript
 cp -r assets ../webprogramming260.github.io/simon-javascript
 cd ../webprogramming260.github.io
 git add .
