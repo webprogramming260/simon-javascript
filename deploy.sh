2aabb1d939d3f7298d83c882fe5b403f9a1cccba
Lee <lee.jensen@agilix.com>
2022-11-28 15:58:02 -0700

    update(deploy) added check to make sure root hostname was installed with caddy

--- deploy.sh
+++ deploy.sh
@@ -1,25 +1,43 @@
-while getopts k:h:a: flag
+#!/bin/bash
+
+while getopts k:h:s: flag
 do
     case "${flag}" in
         k) key=${OPTARG};;
         h) hostname=${OPTARG};;
-        a) assignment=${OPTARG};;
+        s) service=${OPTARG};;
     esac
 done
 
-if [[ -z "$key" || -z "$hostname" || -z "$assignment" ]]; then
+if [[ -z "$key" || -z "$hostname" || -z "$service" ]]; then
     printf "\nMissing required parameter.\n"
-    printf "  syntax: deploy.sh -k <pem key file> -h <hostname> -a <assignment>\n\n"
+    printf "  syntax: deploy.sh -k <pem key file> -h <hostname> -s <service>\n\n"
     exit 1
 fi
 
-printf "\n-------------------------------\nDeploying $assignment to $hostname with $key\n-------------------------------\n"
+hostname=$service.$hostname
 
-# Clear out the previous distribution on the target.
+printf "\n-------------------------------\nDeploying $service to $hostname with $key\n-------------------------------\n"
+
+# Step 1
+printf "\n----> Clear out the previous distribution on the target.\n"
 ssh -i $key ubuntu@$hostname << ENDSSH
-rm -rf public_html/${assignment}
-mkdir -p public_html/${assignment}
+rm -rf public_html/${service}
+mkdir -p public_html/${service}
 ENDSSH
 
-# Copy the distribution package to the target.
-scp -r -i $key * ubuntu@$hostname:public_html/$assignment
+# Step 2
+printf "\n----> Copy the distribution package to the target.\n"
+scp -r -i $key * ubuntu@$hostname:public_html/$service
+
+# Step 3
+printf "\n----> Deploy the service on the target\n"
+ssh -i $key ubuntu@$hostname << ENDSSH
+if cat /etc/caddy/Caddyfile | grep -q ${hostname}; then
+  printf "\n-------------------------------\nUpdating existing service\n-------------------------------\n"
+else
+  printf "\n-------------------------------\nInstalling new service\n-------------------------------\n"
+  sudo sh -c 'printf "\n\n${hostname} {\n\troot * /usr/share/caddy/${service}\n\tfile_server\n}\n" >> Caddyfile'
+  sudo service caddy restart
+fi
+ENDSSH
