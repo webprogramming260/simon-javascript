df491593966d04a08020e4285b8e998459581de6
Lee <lee.jensen@agilix.com>
2023-02-03 08:40:19 -0700

    fix(deploy) allow spaces in key

--- deployFiles.sh
+++ deployFiles.sh
@@ -19,11 +19,11 @@ printf "\n----> Deploying files for $service to $hostname with $key\n"
 
 # Step 1
 printf "\n----> Clear out the previous distribution on the target.\n"
-ssh -i $key ubuntu@$hostname << ENDSSH
+ssh -i "$key" ubuntu@$hostname << ENDSSH
 rm -rf services/${service}/public
 mkdir -p services/${service}/public
 ENDSSH
 
 # Step 2
 printf "\n----> Copy the distribution package to the target.\n"
-scp -r -i $key * ubuntu@$hostname:services/$service/public
+scp -r -i "$key" * ubuntu@$hostname:services/$service/public
