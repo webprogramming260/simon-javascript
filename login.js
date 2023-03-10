3a642c83c328d537455a6801c78e33d44f88ef79
Jaden Taylor <jtjadent@gmail.com>
2023-03-10 15:37:28 -0700

    adding comments

--- login.js
+++ login.js
@@ -1,5 +1,5 @@
-function login() {
-  const nameEl = document.querySelector("#name");
-  localStorage.setItem("userName", nameEl.value);
-  window.location.href = "play.html";
+function login() { 
+  const nameEl = document.querySelector("#name"); //#name refers to the element id in index.html
+  localStorage.setItem("userName", nameEl.value); //localStorage means stores locally on computer
+  window.location.href = "play.html"; //will bring user to a different page
 }
