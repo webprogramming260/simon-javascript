5b4296bee3da182527d5ad0dabc81d3625325159
Jaden Taylor <jtjadent@gmail.com>
2023-03-22 13:47:36 -0600

    updating js

---
+++ public/about.js
@@ -0,0 +1,38 @@
+function displayPicture() {
+    const random = Math.floor(Math.random() * 1000);
+    fetch(`https://picsum.photos/v2/list?page=${random}&limit=1`)
+      .then((response) => response.json())
+      .then((data) => {
+        const containerEl = document.querySelector('#picture');
+  
+        const width = containerEl.offsetWidth;
+        const height = containerEl.offsetHeight;
+  
+        const imgUrl = `https://picsum.photos/id/${data[0].id}/${width}/${height}?grayscale`;
+        const imgEl = document.createElement('img');
+        imgEl.setAttribute('src', imgUrl);
+        containerEl.appendChild(imgEl);
+      });
+  }
+  
+  function displayQuote(data) {
+    fetch('https://api.quotable.io/random')
+      .then((response) => response.json())
+      .then((data) => {
+        const containerEl = document.querySelector('#quote');
+  
+        const quoteEl = document.createElement('p');
+        quoteEl.classList.add('quote');
+        const authorEl = document.createElement('p');
+        authorEl.classList.add('author');
+  
+        quoteEl.textContent = data.content;
+        authorEl.textContent = data.author;
+  
+        containerEl.appendChild(quoteEl);
+        containerEl.appendChild(authorEl);
+      });
+  }
+  
+  displayPicture();
+  displayQuote();
--- public/scores.js
+++ public/scores.js
@@ -1,13 +1,28 @@
-function loadScores() {
+async function loadScores() {
   let scores = [];
-  const scoresText = localStorage.getItem('scores');
-  if (scoresText) {
-    scores = JSON.parse(scoresText);
+  try {
+    // Get the latest high scores from the service
+    const response = await fetch('/api/scores');
+    scores = await response.json();
+
+    // Save the scores in case we go offline in the future
+    localStorage.setItem('scores', JSON.stringify(scores));
+  } catch {
+    // If there was an error then just use the last saved scores
+    const scoresText = localStorage.getItem('scores');
+    if (scoresText) {
+      scores = JSON.parse(scoresText);
+    }
   }
 
+  displayScores(scores);
+}
+
+function displayScores(scores) {
   const tableBodyEl = document.querySelector('#scores');
 
   if (scores.length) {
+    // Update the DOM with the scores
     for (const [i, score] of scores.entries()) {
       const positionTdEl = document.createElement('td');
       const nameTdEl = document.createElement('td');
