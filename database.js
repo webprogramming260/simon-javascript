9d20608dd89f9751b09ea347bfb282900b7824c4
Jaden Taylor <jtjadent@gmail.com>
2023-03-28 18:58:47 -0600

    og

--- database.js
+++ database.js
@@ -1,66 +1,3 @@
-const { MongoClient } = require('mongodb');
-const bcrypt = require('bcrypt');
-const uuid = require('uuid');
-
-const userName = process.env.MONGOUSER;
-const password = process.env.MONGOPASSWORD;
-const hostname = process.env.MONGOHOSTNAME;
-
-if (!userName) {
-  throw Error('Database not configured. Set environment variables');
-}
-
-const url = `mongodb+srv://${userName}:${password}@${hostname}`;
-
-const client = new MongoClient(url);
-const userCollection = client.db('simon').collection('user');
-const scoreCollection = client.db('simon').collection('score');
-
-function getUser(email) {
-  return userCollection.findOne({ email: email });
-}
-
-function getUserByToken(token) {
-  return userCollection.findOne({ token: token });
-}
-
-async function createUser(email, password) {
-  // Hash the password before we insert it into the database
-  const passwordHash = await bcrypt.hash(password, 10);
-
-  const user = {
-    email: email,
-    password: passwordHash,
-    token: uuid.v4(),
-  };
-  await userCollection.insertOne(user);
-
-  return user;
-}
-
-function addScore(score) {
-  scoreCollection.insertOne(score);
-}
-
-function getHighScores() {
-  const query = {};
-  const options = {
-    sort: { score: -1 },
-    limit: 10,
-  };
-  const cursor = scoreCollection.find(query, options);
-  return cursor.toArray();
-}
-
-module.exports = {
-  getUser,
-  getUserByToken,
-  createUser,
-  addScore,
-  getHighScores,
-};
-
-/*
 const {MongoClient} = require('mongodb');
 
 const userName = process.env.MONGOUSER;
@@ -91,4 +28,71 @@ function getHighScores() {
 }
 
 module.exports = {addScore, getHighScores};
+
+/* code with Jensen stuff
+const {MongoClient} = require('mongodb');
+
+const userName = process.env.MONGOUSER;
+const password = process.env.MONGOPASSWORD;
+const hostname = process.env.MONGOHOSTNAME;
+
+if (!userName) {
+  throw Error('Database not configured. Set environment variables');
+}
+
+const url = `mongodb+srv://${userName}:${password}@${hostname}`;
+
+const client = new MongoClient(url);
+//const userCollection = client.db('simon').collection('user'); //this line is in Jensen's code only
+const scoreCollection = client.db('simon').collection('score');
+
+//getUser fucntion, getUserByToken function, and createUser function are all Jensen's code
+
+function getUser(email) {
+    return userCollection.findOne ({ email: email});
+}
+
+function getUserByToken(token) {
+    return userCollection.findOne({ token: token });
+}
+
+async function createUser(email, password) {
+    const passwordHash = await bcrypt.hash(password, 10); //bcrypt is a package from npm that must be installed
+
+    const user = {
+        email: email,
+        password: passwordHash,
+        token: uuid.v4(), //this part might be uuid.v4[]
+};
+await userCollection.insertOne(user);
+
+return user;
+}
+
+
+
+function addScore(score) {
+  scoreCollection.insertOne(score);
+}
+
+function getHighScores() {
+  const query = {score: {$gt: 0}};
+  const options = {
+    sort: {score: -1},
+    limit: 10,
+  };
+  const cursor = scoreCollection.find(query, options);
+  return cursor.toArray();
+}
+
+module.exports = {addScore, getHighScores}; //to comment out if using Jensen's code
+//stuff below in Jensen's code only
+module.exports = {
+    getUser,
+    getUserByToken,
+    createUser,
+    addScore,
+    getHighScores,
+};
+
 */
--- index.js
+++ index.js
@@ -1,131 +1,3 @@
-const cookieParser = require('cookie-parser');
-const bcrypt = require('bcrypt');
-const express = require('express');
-const app = express();
-const DB = require('./database.js');
-
-const authCookieName = 'token';
-
-// The service port may be set on the command line
-const port = process.argv.length > 2 ? process.argv[2] : 3000;
-
-// JSON body parsing using built-in middleware
-app.use(express.json());
-
-// Use the cookie parser middleware for tracking authentication tokens
-app.use(cookieParser());
-
-// Serve up the applications static content
-app.use(express.static('public'));
-
-// Router for service endpoints
-var apiRouter = express.Router();
-app.use(`/api`, apiRouter);
-
-// CreateAuth token for a new user
-apiRouter.post('/auth/create', async (req, res) => {
-  if (await DB.getUser(req.body.email)) {
-    res.status(409).send({ msg: 'Existing user' });
-  } else {
-    const user = await DB.createUser(req.body.email, req.body.password);
-
-    // Set the cookie
-    setAuthCookie(res, user.token);
-
-    res.send({
-      id: user._id,
-    });
-  }
-});
-
-// GetAuth token for the provided credentials
-apiRouter.post('/auth/login', async (req, res) => {
-  const user = await DB.getUser(req.body.email);
-  if (user) {
-    if (await bcrypt.compare(req.body.password, user.password)) {
-      setAuthCookie(res, user.token);
-      res.send({ id: user._id });
-      return;
-    }
-  }
-  res.status(401).send({ msg: 'Unauthorized' });
-});
-
-// DeleteAuth token if stored in cookie
-apiRouter.delete('/auth/logout', (_req, res) => {
-  res.clearCookie(authCookieName);
-  res.status(204).end();
-});
-
-// GetUser returns information about a user
-apiRouter.get('/user/:email', async (req, res) => {
-  const user = await DB.getUser(req.params.email);
-  if (user) {
-    const token = req?.cookies.token;
-    res.send({ email: user.email, authenticated: token === user.token });
-    return;
-  }
-  res.status(404).send({ msg: 'Unknown' });
-});
-
-// secureApiRouter verifies credentials for endpoints
-var secureApiRouter = express.Router();
-apiRouter.use(secureApiRouter);
-
-secureApiRouter.use(async (req, res, next) => {
-  authToken = req.cookies[authCookieName];
-  const user = await DB.getUserByToken(authToken);
-  if (user) {
-    next();
-  } else {
-    res.status(401).send({ msg: 'Unauthorized' });
-  }
-});
-
-// GetScores
-secureApiRouter.get('/scores', async (req, res) => {
-  const scores = await DB.getHighScores();
-  res.send(scores);
-});
-
-// SubmitScore
-secureApiRouter.post('/score', async (req, res) => {
-  await DB.addScore(req.body);
-  const scores = await DB.getHighScores();
-  res.send(scores);
-});
-
-// Default error handler
-app.use(function (err, req, res, next) {
-  res.status(500).send({ type: err.name, message: err.message });
-});
-
-// Return the application's default page if the path is unknown
-app.use((_req, res) => {
-  res.sendFile('index.html', { root: 'public' });
-});
-
-// setAuthCookie in the HTTP response
-function setAuthCookie(res, authToken) {
-  res.cookie(authCookieName, authToken, {
-    secure: true,
-    httpOnly: true,
-    sameSite: 'strict',
-  });
-}
-
-app.listen(port, () => {
-  console.log(`Listening on port ${port}`);
-});
-
-
-
-
-
-
-
-
-/* oldish code
 const express = require('express');
 const app = express();
 const DB = require('./database.js');
@@ -164,4 +36,170 @@ app.use((_req, res) => {
 app.listen(port, () => {
   console.log(`Listening on port ${port}`);
 });
+
+
+/* new code with Jensen stuff
+const express = require('express');
+const app = express();
+const DB = require('./database.js');
+
+// The service port. In production the application is statically hosted by the service on the same port.
+const port = process.argv.length > 2 ? process.argv[2] : 3000;
+
+// JSON body parsing using built-in middleware
+app.use(express.json());
+
+// Serve up the applications static content
+app.use(express.static('public'));
+
+// Router for service endpoints
+var apiRouter = express.Router();
+app.use(`/api`, apiRouter);
+
+// GetScores
+apiRouter.get('/scores', async (_req, res) => {
+  const scores = await DB.getHighScores();
+  res.send(scores);
+});
+
+
+
+// SubmitScore
+apiRouter.post('/score', async (req, res) => {
+  DB.addScore(req.body);
+  const scores = await DB.getHighScores();
+  res.send(scores);
+});
+
+//Jensen's code
+apiRouter,post('/auth/create', async (req, res) => {
+  if (await Db.getuser(req.body.email)) {
+    res.status(409).send({ msg: 'Existing user'});
+  } else {
+    const user = await DB.createUser(req.body.email, req.body.password);
+
+    //set the cookie
+    setAuthCookie(res, user.token);
+
+    res.send({
+      id: user._id,
+    });
+  }
+})
+
+
+apiRouter.post('/auth/login', async (req, res) => {
+  const user = await DB.getUser{req.body.email};
+  if (user) {
+    if (await bcrypt.compare(req.body.password, user.password)) {
+      setAuthCookie(res, user.token);
+      res.send({ id: user._id});
+      return;
+    }
+  }
+  res.status(401).send({ msg: 'Unauthorized'});
+});
+
+apirouter.delete('/auth/logout', (_req, res) => {
+  res.clearCookie(authCookieName);
+  res.status(204).end();
+});
+
+
+// Return the application's default page if the path is unknown
+app.use((_req, res) => {
+  res.sendFile('index.html', {root: 'public'});
+});
+
+//Jensen's code
+function setAuthCookie(res, authToken) {
+  res.cookie(setAuthCookie, authToken, {
+    secure: true,
+    httpOnly: true,
+    sameSite: 'strict',
+  });
+}
+
+
+
+app.listen(port, () => {
+  console.log(`Listening on port ${port}`);
+});
+
+*/
+
+
+
+
+
+
+
+
+
+
+
+/* old code
+const express = require('express');
+const app = express();
+
+// The service port. In production the application is statically hosted by the service on the same port.
+const port = process.argv.length > 2 ? process.argv[2] : 3000;
+//this part is important. For the startup, do port 4000 instead.
+
+// JSON body parsing using built-in middleware
+app.use(express.json());
+
+// Serve up the application's static content
+app.use(express.static('public'));
+//this tells it to access the public folder
+
+// Router for service endpoints
+var apiRouter = express.Router();
+app.use(`/api`, apiRouter);
+
+// GetScores
+apiRouter.get('/scores', (_req, res) => {
+  res.send(scores);
+});
+
+// SubmitScore
+apiRouter.post('/score', (req, res) => {
+  scores = updateScores(req.body, scores);
+  res.send(scores);
+});
+
+// Return the application's default page if the path is unknown
+app.use((_req, res) => {
+  res.sendFile('index.html', { root: 'public' });
+});
+
+app.listen(port, () => {
+  console.log(`Listening on port ${port}`);
+});
+
+// updateScores considers a new score for inclusion in the high scores.
+// The high scores are saved in memory and disappear whenever the service is restarted.
+let scores = [];
+function updateScores(newScore, scores) {
+  let found = false;
+  for (const [i, prevScore] of scores.entries()) {
+    if (newScore.score > prevScore.score) {
+      scores.splice(i, 0, newScore);
+      found = true;
+      break;
+    }
+  }
+
+  if (!found) {
+    scores.push(newScore);
+  }
+
+  //this allows you to limit how many scores are displayed
+  //i.e. top 10, top 15, etc.
+  if (scores.length > 10) {
+    scores.length = 15;
+  }
+
+  return scores;
+}
 */
