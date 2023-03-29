a6df1d1dfc42d293c0b44bdff5ffe6d2bed072d7
Jaden Taylor <jtjadent@gmail.com>
2023-03-28 19:25:44 -0600

    The real deal

--- database.js
+++ database.js
@@ -1,4 +1,6 @@
-const {MongoClient} = require('mongodb');
+const { MongoClient } = require('mongodb');
+const bcrypt = require('bcrypt');
+const uuid = require('uuid');
 
 const userName = process.env.MONGOUSER;
 const password = process.env.MONGOPASSWORD;
@@ -8,21 +10,52 @@ if (!userName) {
   throw Error('Database not configured. Set environment variables');
 }
 
-const url = `mongodb+srv://${userName}:${password}@cluster0.uma6uu5.mongodb.net/`;
+const url = `mongodb+srv://${userName}:${password}@${hostname}`;
 
 const client = new MongoClient(url);
+const userCollection = client.db('simon').collection('user');
 const scoreCollection = client.db('simon').collection('score');
+
+function getUser(email) {
+  return userCollection.findOne({ email: email });
+}
+
+function getUserByToken(token) {
+  return userCollection.findOne({ token: token });
+}
+
+async function createUser(email, password) {
+  // Hash the password before we insert it into the database
+  const passwordHash = await bcrypt.hash(password, 10);
+
+  const user = {
+    email: email,
+    password: passwordHash,
+    token: uuid.v4(),
+  };
+  await userCollection.insertOne(user);
+
+  return user;
+}
 
 function addScore(score) {
   scoreCollection.insertOne(score);
 }
 
 function getHighScores() {
-  const query = {score: {$gt: 0}};
+  const query = {};
   const options = {
-    sort: {score: -1},
+    sort: { score: -1 },
     limit: 10,
   };
   const cursor = scoreCollection.find(query, options);
   return cursor.toArray();
 }
+
+module.exports = {
+  getUser,
+  getUserByToken,
+  createUser,
+  addScore,
+  getHighScores,
+};
