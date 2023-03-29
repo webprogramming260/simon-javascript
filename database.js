7bee24f93dd0ed312d86f579259d5ac697c48afe
Jaden Taylor <jtjadent@gmail.com>
2023-03-28 19:10:45 -0600

    Updating to add a login password

--- database.js
+++ database.js
@@ -59,36 +59,3 @@ module.exports = {
   addScore,
   getHighScores,
 };
-
-/*
-const {MongoClient} = require('mongodb');
-
-const userName = process.env.MONGOUSER;
-const password = process.env.MONGOPASSWORD;
-const hostname = process.env.MONGOHOSTNAME;
-
-if (!userName) {
-  throw Error('Database not configured. Set environment variables');
-}
-
-const url = `mongodb+srv://${userName}:${password}@cluster0.uma6uu5.mongodb.net/`;
-
-const client = new MongoClient(url);
-const scoreCollection = client.db('simon').collection('score');
-
-function addScore(score) {
-  scoreCollection.insertOne(score);
-}
-
-function getHighScores() {
-  const query = {score: {$gt: 0}};
-  const options = {
-    sort: {score: -1},
-    limit: 10,
-  };
-  const cursor = scoreCollection.find(query, options);
-  return cursor.toArray();
-}
-
-module.exports = {addScore, getHighScores};
-*/
