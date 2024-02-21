cbaf48beea3210d865a898db06b7e58e137ee8bd
Lee <leesjensen@gmail.com>
2024-02-21 05:26:49 -0700

    enhance(play) with comment

--- play.js
+++ play.js
@@ -179,9 +179,11 @@ function loadSound(filename) {
   return new Audio('assets/' + filename);
 }
 
+// Simulate chat messages that will come over WebSocket
 setInterval(() => {
   const score = Math.floor(Math.random() * 3000);
   const chatText = document.querySelector('#player-messages');
   chatText.innerHTML =
-    `<div class="event"><span class="player-event">Eich</span> scored ${score}</div>` + chatText.innerHTML;
+    `<div class="event"><span class="player-event">Eich</span> scored ${score}</div>` +
+    chatText.innerHTML;
 }, 5000);
